d8ce6ea9631807a3136e618572df0bed876cade0
sirk007 <147782045+sirk007@users.noreply.github.com>
2026-09-11 14:35:31 +0100

    fix error 404
    corrected yaml file

--- my-portfolio/next.config.ts
+++ my-portfolio/next.config.ts
@@ -2,9 +2,8 @@
 const nextConfig = {
   output: "export",
   images: { unoptimized: true },
-  basePath: process.env.NODE_ENV === "production" ? "/Nextjs-portfolio" : "",
-  assetPrefix:
-    process.env.NODE_ENV === "production" ? "/Nextjs-portfolio/" : "",
+  basePath: "/Nextjs-portfolio",
+  assetPrefix: "/Nextjs-portfolio/",
   trailingSlash: true,
 };
 
